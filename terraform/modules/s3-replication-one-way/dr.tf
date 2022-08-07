ed5d137df4b0bd6ef757d7efb7d44c5f410d1f24
Daniel Lloyd <daniel.lloyd@foghornconsulting.com>
2022-08-07 13:44:23 -0500

    TFC s3 rep testing

--- terraform/modules/s3-replication-one-way/dr.tf
+++ terraform/modules/s3-replication-one-way/dr.tf
@@ -2,9 +2,131 @@ variable "primary_remote_state" {
   default = null
 }
 
-# provider "aws" {
-#   alias = "primary"
-# }
+provider "aws" {
+  alias = "primary"
+}
+
+###########
+## IF DR ##
+###########
+# inputs: 
+# AWS provider for us-east-1
+# Primary bucket ARN:   data.terraform_remote_state.primary.outputs.rep_test_bucket.rep_test_bucket.arn
+# DR bucket ARN:        module.s3_rep_test_dr.rep_test_bucket.arn
+# DR bucket ID (name):  data.terraform_remote_state.primary.outputs.rep_test_bucket.rep_test_bucket.id
+
+resource "aws_iam_role" "s3_replication" {
+  count = var.dr_enabled ? 1 : 0
+
+  name = "S3ReplicationTest"
+
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": "sts:AssumeRole",
+        "Principal": {
+          "Service": "s3.amazonaws.com"
+        },
+        "Effect": "Allow",
+        "Sid": ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy" "s3_replication" {
+  count = var.dr_enabled ? 1 : 0
+
+  name = "S3ReplicationTest"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+      {
+        "Action": [
+          "s3:GetReplicationConfiguration",
+          "s3:ListBucket"
+        ],
+        "Effect": "Allow",
+        "Resource": [
+          "${primary_remote_state.rep_test_bucket.rep_test_bucket.arn}"
+        ]
+      },
+      {
+        "Action": [
+          "s3:GetObjectVersionForReplication",
+          "s3:GetObjectVersionAcl",
+          "s3:GetObjectVersionTagging"
+        ],
+        "Effect": "Allow",
+        "Resource": [
+          "${primary_remote_state.rep_test_bucket.rep_test_bucket.arn}/*"
+        ]
+      },
+      {
+        "Action": [
+          "s3:ReplicateObject",
+          "s3:ReplicateDelete",
+          "s3:ReplicateTags"
+        ],
+        "Effect": "Allow",
+        "Resource": "${aws_s3_bucket.rep_test.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "replication" {
+  count = var.dr_enabled ? 1 : 0
+
+  role = aws_iam_role.s3_replication[0].name
+  policy_arn = aws_iam_policy.s3_replication[0].arn
+}
+
+resource "aws_s3_bucket_replication_configuration" "replication" {
+  count = var.dr_enabled ? 1 : 0
+  provider = aws.primary
+  
+  # Must have bucket versioning enabled first
+  depends_on = [aws_s3_bucket_versioning.rep_test]
+
+  role = aws_iam_role.s3_replication[0].arn
+  bucket = primary_remote_state.rep_test_bucket.rep_test_bucket.id
+
+  rule {
+    status = "Enabled"
+
+    destination {
+      bucket = aws_s3_bucket_versioning.rep_test.arn
+      storage_class = "STANDARD"
+    }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # data "aws_s3_bucket" "rep_test_primary" {
 #   count = var.dr_enabled ? 1 : 0
--- terraform/s3-replication-testing/dr/main.tf
+++ terraform/s3-replication-testing/dr/main.tf
@@ -21,96 +21,10 @@ data "terraform_remote_state" "primary" {
 module "s3_rep_test_dr" {
   source = "../../modules/s3-replication-one-way"
 
+  providers = {
+    aws = aws.primary
+  }
+
   dr_enabled = true
   primary_remote_state = data.terraform_remote_state.primary.outputs
-}
-
-###########
-## IF DR ##
-###########
-# inputs: 
-# AWS provider for us-east-1
-# Primary bucket ARN:   data.terraform_remote_state.primary.outputs.rep_test_bucket.rep_test_bucket.arn
-# DR bucket ARN:        module.s3_rep_test_dr.rep_test_bucket.arn
-# DR bucket ID (name):  data.terraform_remote_state.primary.outputs.rep_test_bucket.rep_test_bucket.id
-
-resource "aws_iam_role" "s3_replication" {
-  name = "S3ReplicationTest"
-
-  assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": "sts:AssumeRole",
-        "Principal": {
-          "Service": "s3.amazonaws.com"
-        },
-        "Effect": "Allow",
-        "Sid": ""
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "s3_replication" {
-  name = "S3ReplicationTest"
-
-  policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-      {
-        "Action": [
-          "s3:GetReplicationConfiguration",
-          "s3:ListBucket"
-        ],
-        "Effect": "Allow",
-        "Resource": [
-          "${data.terraform_remote_state.primary.outputs.rep_test_bucket.rep_test_bucket.arn}"
-        ]
-      },
-      {
-        "Action": [
-          "s3:GetObjectVersionForReplication",
-          "s3:GetObjectVersionAcl",
-          "s3:GetObjectVersionTagging"
-        ],
-        "Effect": "Allow",
-        "Resource": [
-          "${data.terraform_remote_state.primary.outputs.rep_test_bucket.rep_test_bucket.arn}/*"
-        ]
-      },
-      {
-        "Action": [
-          "s3:ReplicateObject",
-          "s3:ReplicateDelete",
-          "s3:ReplicateTags"
-        ],
-        "Effect": "Allow",
-        "Resource": "${module.s3_rep_test_dr.rep_test_bucket.arn}/*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "replication" {
-  role = aws_iam_role.s3_replication.name
-  policy_arn = aws_iam_policy.s3_replication.arn
-}
-
-resource "aws_s3_bucket_replication_configuration" "replication" {
-  provider = aws.primary
-  # Must have bucket versioning enabled first
-  # depends_on = [aws_s3_bucket_versioning.source]
-
-  role = aws_iam_role.s3_replication.arn
-  bucket = data.terraform_remote_state.primary.outputs.rep_test_bucket.rep_test_bucket.id
-
-  rule {
-    status = "Enabled"
-
-    destination {
-      bucket = module.s3_rep_test_dr.rep_test_bucket.arn
-      storage_class = "STANDARD"
-    }
-  }
 }
