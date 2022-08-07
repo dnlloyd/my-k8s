c5617149105cbd855ef5c32054709edede8d0eb0
Daniel Lloyd <daniel.lloyd@foghornconsulting.com>
2022-08-07 09:46:55 -0500

    TFC s3 rep testing

--- terraform/s3-replication-testing/main.tf
+++ terraform/s3-replication-testing/main.tf
@@ -2,22 +2,12 @@ provider "aws" {
   region  = "us-east-1"
 }
 
-# provider "aws" {
-#   alias   = "dr"
-#   region  = "us-east-2"
-# }
-
 module "s3_rep_test" {
   source = "../modules/s3-replication-one-way"
-
-  # providers = {
-  #   aws = aws.dr
-  # }
 
   name = "primary"
 }
 
-output "primary_bucket_name" {
+output "rep_test_bucket_name" {
   value = module.s3_rep_test.bucket_name
 }
-#trigger 01
